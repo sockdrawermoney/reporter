d328e4483bd39a4f81fa33fb8888062868f862ef
Adam Avenir <adam@andyet.com>
2021-04-02 15:41:15 -0700

    form cleanup

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import { omit, find } from "lodash";
 import config from "../config.json";
 import history from "../utils/history";
-import createIssue from "../functions/createIssue";
 import { Widgets } from "./widgets";
 
-const contest = process.env.CONTEST;
-const sponsor = process.env.SPONSOR;
+const contest = process.env.REACT_APP_C4_CONTEST;
+const sponsor = process.env.REACT_APP_C4_SPONSOR;
 
 const Form = () => {
   const [state, setState] = useState({
@@ -21,6 +20,35 @@ const Form = () => {
       ...state,
       [target.name]: target.value,
     });
+  };
+
+  const url = `/.netlify/functions/submit-finding`;
+
+  const submitFinding = async (url, data) => {
+    setState({
+      ...state,
+      status: "submitting",
+    });
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (response.ok) {
+      setState({
+        ...state,
+        status: "submitted",
+      });
+      return history.push("/success");
+    } else {
+      history.push("/error");
+      setState({
+        ...state,
+        status: "error",
+      });
+      const message = `Error: ${response.status}`;
+      throw new Error(message);
+    }
   };
 
   // body contains everything in state except title, label, and status
@@ -39,33 +67,28 @@ const Form = () => {
     state.label ? state.label : "",
   ];
 
+  let risk;
+  if (state.label) {
+    risk = state.label.slice(0, 1);
+  }
+
   const formData = {
-    contest,
-    sponsor,
     email: state.email,
     handle: state.handle,
-    risk: state.risk,
+    risk,
     title: state.title,
     body: markdownBody.join("\n"),
     labels: labelSet,
   };
 
   const handleSubmit = () => {
-    createIssue(formData).then((result) => {
-      if (!result.statusCode || result.statusCode === 200) {
-        setState({
-          ...state,
-          status: "submitted",
-        });
-        return history.push("/success");
-      } else {
-        console.error(result.statusCode, result.message);
-        history.push("/500");
-        setState({
-          ...state,
-          status: "error",
-        });
-      }
+    submitFinding(url, formData);
+  };
+
+  const handleReset = () => {
+    history.push("/");
+    setState({
+      status: "unsubmitted",
     });
   };
 
@@ -74,19 +97,17 @@ const Form = () => {
       <a href="https://code423n4.com">
         <img src="/logo.png" className="logo" width="60" alt="C4" />
       </a>
-      <h1>{config.sponsor} contest finding</h1>
+      <h1>{sponsor} contest finding</h1>
       <form>
-        <input
-          type="hidden"
-          id="contest"
-          name="contest"
-          value={config.contest}
-        />
+        <input type="hidden" id="contest" name="contest" value={contest} />
         <Widgets fields={fields} onChange={handleChange} />
         <button type="button" onClick={handleSubmit}>
           Create issue
         </button>
       </form>
+      <button className="submit-another" type="button" onClick={handleReset}>
+        Submit another
+      </button>
     </div>
   );
 };

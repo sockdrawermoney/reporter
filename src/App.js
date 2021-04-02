528da11d9f1f2ab266610e9230d9bf7c2dba205b
Adam Avenir <adam@andyet.com>
2021-04-02 15:50:42 -0700

    page title

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 import "./styles/App.css";
 import Form from "./components/Form";
 import DefaultLayout from "./layouts/DefaultLayout";
-import config from "./config.json";
-
-const sponsor = config.sponsor;
 
 function App() {
   return (
-    <DefaultLayout pageTitle={"C4 " + sponsor + " contest finding"}>
+    <DefaultLayout pageTitle={"C4 contest finding submission"}>
       <div className="App">
         <Form />
       </div>
